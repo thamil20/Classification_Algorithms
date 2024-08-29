3da6f578f2fe264b086145668ed2d881d8ebc296
Tyler Hamilton <hamiltontyler3900@icloud.com>
2024-08-29 19:25:23 -0400

    Saving Progress

--- Dog_Vs_Cat/main.py
+++ Dog_Vs_Cat/main.py
@@ -2,32 +2,62 @@ import tensorflow as tf
 import os
 import numpy as np
 from matplotlib import pyplot as plt
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 # Limit GPUs memory growth, avoiding OOM errors.
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-# Remove unnecessary images.
 data_dir = "Dog_Vs_Cat\\dataset"
-image_exts = ["png"]
 
-for image_class in os.listdir(data_dir):
-    for image in os.listdir(os.path.join(data_dir, image_class)):
-        image_path = os.path.join(data_dir, image_class, image)
-        pass
-    
+########################################################################
 
-data = tf.keras.utils.image_dataset_from_directory(data_dir, batch_size=16)
+# Loading Data into TensorFlow
+data = tf.keras.utils.image_dataset_from_directory(data_dir, batch_size=8)
 data_iterator = data.as_numpy_iterator()
+
+# Get another batch from the iterator
 batch = data_iterator.next()
+
 print(batch[0].shape)
+
+# 1 = Cats, 0 = Dogs
 print(batch[1])
 
-# fig, ax, = plt.subplots(ncols=4, figsize=(20,20))
-# for idx, img in enumerate(batch[0][:4]):
-#     ax[idx].imshow(img.astype(int))
-#     ax[idx].title.set_text(batch[1][idx])
-# plt.show()
 
+def plotting(batch):
+    fig, ax, = plt.subplots(ncols=4, figsize=(20,20))
+    for idx, img in enumerate(batch[0][:4]):
+        ax[idx].imshow(img)
+        ax[idx].title.set_text(batch[1][idx])
+    plt.show()
+
+########################################################################
+
+# Data Preprocessing
+data = data.map(lambda x,y: (x/255, y))
+scaled_iterator = data.as_numpy_iterator().next()
+
+# plotting(scaled_iterator)
+
+# Splitting Data
+# 70% of data in training
+training_size = int(len(data)*.7)
+# 20% of data in validating
+validating_size = int(len(data)*.2)
+# 10% of data in testing
+testing_size = int(len(data)*.1)
+
+# Check how much data was split. 
+# print(testing_size, validating_size, training_size)\
+
+# In this case, 8 pictures were not split amongst the dataset. 
+# There are 992 pictures in this dataset.
+
+
+# Take or skip all data within the data set.
+training = data.take(training_size)
+validating = data.skip(training_size).take(validating_size)
+testing = data.skip(training_size+validating_size).take(testing_size)
